ed3b840facdd3dd5002c2b47150fdc0c57ea2e97
Nick Sullivan <nbsullivan@gmail.com>
2016-06-14 15:47:32 -0700

    adding paper outline

---
+++ acceptrejectpaper.m
@@ -0,0 +1,18 @@
+%% The Accept Reject Method: Anthony Gusman, Nicholas Sullivan
+
+%% Introduction
+% The process of generating pseudo random numbers using a given cumlative
+% density function of a random variable is easy if the inverse of the CDF
+% is known or quick to find.
+
+
+%% Overview of Method
+% write out algo in plain english
+
+%% Break down of function code
+% review the function block by block
+
+%% Examples
+% Lets do the beta and someother unhappy distribution
+
+%% Full function code
