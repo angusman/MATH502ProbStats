4e5690e082fb3bcf689ffd3154d31fbae77cd019
angusman <agusman@csu.fullerton.edu>
2016-06-17 17:35:09 -0700

    minor index adjustments

--- randPDF.m
+++ randPDF.m
@@ -1,8 +1,12 @@
 function [x] = randPDF( fun, a, b, n)
-%Generate  1 x n  a vectorof random numbers following the distribution of a
-%Given PDF.
+% RANDPDF Generate random numbers from user-specified distribution. 
+%
+% Produces a 1 x n vector of random numbers following the distribution of a
+% given PDF.
+%
 %   Output:
 %   x: 1 x n vector of random numbers following the distribution of fun
+%
 %   Input:
 %   fun: a PDF function with compact support over [a,b]
 %   a: the lower bound for the support of fun
@@ -10,26 +14,26 @@ function [x] = randPDF( fun, a, b, n)
 %   n: the desired number of random values
 
 x = zeros(1,n); % placeholder for generated numbers
-filled = 0; % keeping track of the accepted values
+filled = 0;     % keeping track of the accepted values
 
-% find ratio of area beneather pdf to box size
+% Find ratio of area beneath pdf to box size.
 I = integral(fun,a,b);
 f = @(x) (1/I)*fun(x);
 c = max([f(a),f(b),f(fminbnd(@(x) -f(x),a,b))]);     % locate maximum
 boxsize = ceil(c*(b-a));
 
-
+% Fill the output array with the desired amount of random numbers.
 filled = 1;
 oldfill = 1;
 x = zeros(1,n);
-while filled < n 
+while filled <= n 
     A = acceptReject(f,a,b, ((n - filled)*boxsize));
     oldfill = filled;
     filled = filled + length(A);
     if filled <= n
-        x(1,oldfill:filled-1) = A;
+        x(oldfill:filled-1) = A;
     else
-        x(1,oldfill:n) = A(1:n-oldfill+1);
+        x(oldfill:n) = A(1:n-oldfill+1);
     end
 end
 
